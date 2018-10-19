513cec6a45b7e9d010605f8d6bed9e3f346dac18
Andy Iwamoto <aiwamoto789@gmail.com>
2018-10-19 14:07:23 -0600

    stabbed entities

---
+++ JPARoster/src/main/java/com/skilldistillery/JPARoster/entities/JPAEntities.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.JPARoster.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class JPAEntities {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int player_id;
+	private String player_name;
+	private String position;
+}
